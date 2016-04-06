f985265a7d193766c5111d2c5fba2eb0462777d2
Michael Ta <technical.ta@gmail.com>
2016-04-06 16:04:46 -0400

    Added new dependencies. Added utils. Created pinger service. Created monitor service. ---Need to write tests

---
+++ config/storage.js
@@ -0,0 +1,12 @@
+module.exports = {
+	development: {
+		provider: 'redis',
+		options: {
+			'redis': {
+				port: process.env.MONITOR_REDIS_PORT_DEV || 1216,
+				host: process.env.MONITOR_REDIS_ADDR_DEV || '127.0.0.1',
+				db: process.env.MONITOR_REDIS_DB_DEV || 2
+			}
+		}
+	}
+};
