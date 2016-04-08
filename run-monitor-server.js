5ba45c697288e3fdd38fc6bf1371ddb360004e93
Michael Ta <technical.ta@gmail.com>
2016-04-08 10:46:48 -0400

    Monitor/Pinger/Console reporter is working properly.  Need to add in actual storage solution for status and save downtime.

--- run-monitor-server.js
+++ run-monitor-server.js
@@ -2,6 +2,7 @@ var MonitorFactory = require('./lib/monitor');
 var consoleUtil = require('./lib/console-util'); 
 var MockedStorage = require('./test/mock/storage-mocked');
 var moment = require('moment');
+var commander = require('commander');
 
 var RETURN_CODES = {
   OK: 0,
@@ -9,21 +10,56 @@ var RETURN_CODES = {
   GENERIC_ERROR: 2
 };
 
-var service = {
-			id: 'X34dF',
-			host: {
-				host: 'http://www.phiresearchlab.org',
-				port: '80',
-				name: 'IIU Website'
-			},
-			url: '/',
-			interval: 4 * 1000,
-			failureInterval: 5 * 1000,
-			warninThreshold: 1500
-		};
+var services = [
+		{
+			name: 'IIU Lab Website',
+			interval: 30 * 1000,
+			failureInterval: 20 * 1000,
+			url: 'http://www.phiresearchlab.org',
+			port: '80',
+			timeout: 10000,
+			warningThreshold: 3000
+		},
+		{
+			name: 'Google',
+			interval: 30 * 1000,
+			failureInterval: 20 * 1000,
+			url: 'http://www.google.com',
+			port: '80',
+			timeout: 10000,
+			warningThreshold: 3000
+		},
+		{
+			name: 'App Lab',
+			interval: 30 * 1000,
+			failureInterval: 20 * 1000,
+			url: 'http://applab.phiresearchlab.org',
+			port: '80',
+			timeout: 10000,
+			warningThreshold: 3000
+		},
+		{
+			name: 'App Lab Dev',
+			interval: 30 * 1000,
+			failureInterval: 20 * 1000,
+			url: 'http://applpydev.phiresearchlab.org',
+			port: '80',
+			timeout: 10000,
+			warningThreshold: 3000
+		}
+];
 
-var monitor = new MonitorFactory([service], new MockedStorage(null));
+commander
+	.option('-e, --env [env]', 'Storage environment key', process.env.NODE_ENV || 'development')
+	.option('-d, --max-initial-delay [value]', 'Initial random delay max bound', 10000)
+	.parse(process.argv);
 
+var monitor = new MonitorFactory(services, new MockedStorage(null));
+
+consoleUtil(monitor);
+
+monitor.startAll({randomDelayOnInit: commander.maxInitialDelay});
+console.log('Starting monitor ' + services.length +' services loaded');
 
 
 
