b600fee779a86030950d8cc7c508be10508fe97d
Michael Ta <technical.ta@gmail.com>
2016-05-02 16:29:02 -0400

    rearraged dashboard, cleanup

--- src/server/api/jira.js
+++ src/server/api/jira.js
@@ -27,6 +27,7 @@ module.exports.getRoutes = function() {
 
 		request(reservationsQuery, auth, function(error,response, body) {
 			if(!error && response.statusCode === 200) {
+				var dateNow = +new Date();
 				var parsedObj = JSON.parse(body);
 				var roomReservationArray = [];
 				parsedObj = parsedObj.issues;
@@ -105,99 +106,19 @@ module.exports.getRoutes = function() {
 						roomReservationArray.push(date6Obj);
 					}
 				}
-				var sortedArray = _.orderBy(roomReservationArray, ['reservedDate'], ['asc']);
-				return res.send(sortedArray);
+				var filteredArray = [];
+				for(var j=0; j < roomReservationArray.length; j++){
+					if(roomReservationArray[j].reservedDate >= dateNow){
+						filteredArray.push(roomReservationArray[j]);
+					}
+				}
+				filteredArray = _.orderBy(filteredArray, ['reservedDate'], ['asc']);
+				return res.send(filteredArray);
 			} else {
 				console.log(error);
 			}
 		});
-		// request(host+path, auth, function(error, response, body) {   //REFACTOR, DEAR GOD REFACTOR
-		// 	if (!error && response.statusCode === 200) {
-		// 		var requestId = '10103';
-		// 		var roomReservationArray = [];
-		// 		var dateNow = +new Date();
-		// 						// console.log(body);
-		// 		var parsedObj = JSON.parse(body);
-		// 		parsedObj = parsedObj.issues;
-		// 		for(var i =0; i < parsedObj.length; i++) {
-		// 			var issueObj = {};
-		// 			if(parsedObj[i].fields.issuetype.id === requestId) {
-		// 				if(parsedObj[i].fields.customfield_10863 !==null){
-		// 					issueObj.id = parsedObj[i].id;
-		// 					issueObj.key = parsedObj[i].key;
-		// 					issueObj.priority = parsedObj[i].fields.priority.name;
-		// 					issueObj.status = parsedObj[i].fields.status.name;
-		// 					issueObj.type = parsedObj[i].fields.issuetype.name;
-		// 					issueObj.summary = parsedObj[i].fields.summary;
-		// 					issueObj.reservedDate = Date.parse(parsedObj[i].fields.customfield_10863);
-		// 					roomReservationArray.push(issueObj);
-		// 				}
-		// 				if(parsedObj[i].fields.customfield_11102 !==null){
-		// 					issueObj.id = parsedObj[i].id;
-		// 					issueObj.key = parsedObj[i].key;
-		// 					issueObj.priority = parsedObj[i].fields.priority.name;
-		// 					issueObj.status = parsedObj[i].fields.status.name;
-		// 					issueObj.type = parsedObj[i].fields.issuetype.name;
-		// 					issueObj.summary = parsedObj[i].fields.summary;
-		// 					issueObj.reservedDate = Date.parse(parsedObj[i].fields.customfield_11102);
-		// 					roomReservationArray.push(issueObj);
-		// 				}
-		// 				if(parsedObj[i].fields.customfield_11103 !==null){
-		// 					issueObj.id = parsedObj[i].id;
-		// 					issueObj.key = parsedObj[i].key;
-		// 					issueObj.priority = parsedObj[i].fields.priority.name;
-		// 					issueObj.status = parsedObj[i].fields.status.name;
-		// 					issueObj.type = parsedObj[i].fields.issuetype.name;
-		// 					issueObj.summary = parsedObj[i].fields.summary;
-		// 					issueObj.reservedDate = Date.parse(parsedObj[i].fields.customfield_11103);
-		// 					roomReservationArray.push(issueObj);
-		// 				}
-		// 				if(parsedObj[i].fields.customfield_11104 !==null){
-		// 					issueObj.id = parsedObj[i].id;
-		// 					issueObj.key = parsedObj[i].key;
-		// 					issueObj.priority = parsedObj[i].fields.priority.name;
-		// 					issueObj.status = parsedObj[i].fields.status.name;
-		// 					issueObj.type = parsedObj[i].fields.issuetype.name;
-		// 					issueObj.summary = parsedObj[i].fields.summary;
-		// 					issueObj.reservedDate = Date.parse(parsedObj[i].fields.customfield_11104);
-		// 					roomReservationArray.push(issueObj);
-		// 				}
-		// 				if(parsedObj[i].fields.customfield_11105 !==null){
-		// 					issueObj.id = parsedObj[i].id;
-		// 					issueObj.key = parsedObj[i].key;
-		// 					issueObj.priority = parsedObj[i].fields.priority.name;
-		// 					issueObj.status = parsedObj[i].fields.status.name;
-		// 					issueObj.type = parsedObj[i].fields.issuetype.name;
-		// 					issueObj.summary = parsedObj[i].fields.summary;
-		// 					issueObj.reservedDate = Date.parse(parsedObj[i].fields.customfield_11105);
-		// 					roomReservationArray.push(issueObj);
-		// 				}
-		// 				if(parsedObj[i].fields.customfield_11106!==null){
-		// 					issueObj.id = parsedObj[i].id;
-		// 					issueObj.key = parsedObj[i].key;
-		// 					issueObj.priority = parsedObj[i].fields.priority.name;
-		// 					issueObj.status = parsedObj[i].fields.status.name;
-		// 					issueObj.type = parsedObj[i].fields.issuetype.name;
-		// 					issueObj.summary = parsedObj[i].fields.summary;
-		// 					issueObj.reservedDate = Date.parse(parsedObj[i].fields.customfield_11106);
-		// 					roomReservationArray.push(issueObj);
-		// 				}
-		// 			}
-		// 		}
-		// 		var filteredArray = [];
-		// 		for(var j=0; j < roomReservationArray.length; j++){
-		// 			if(roomReservationArray[j].reservedDate > dateNow){
-		// 				filteredArray.push(roomReservationArray[j]);
-		// 			}
-		// 		}
-		// 		// console.log('Array Post filter', roomReservationArray);
-
-		// 		filteredArray = _.orderBy(filteredArray, ['reservedDate'], ['asc']);
-		// 		return res.send(filteredArray);
-		// 	} else {
-		// 		console.log(error);
-		// 	}
-		// });
+		
 	});
 
 
