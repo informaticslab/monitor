135bf44abba91d52e165efbe859c3204342f3045
Michael Ta <technical.ta@gmail.com>
2016-05-02 15:29:28 -0400

    cleanup

--- src/client/app/components/home/home.component.ts
+++ src/client/app/components/home/home.component.ts
@@ -2,9 +2,7 @@ import {Component, ViewEncapsulation} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {TopnavComponent} from '../topnav/topnav.component';
 // import {SidebarComponent} from '../sidebar/sidebar.component';
-// import {ReportComponent} from '../reports/report.component';
 import {DashboardComponent} from '../dashboard/dashboard.component';
-// import {bootstrap} from 'angular2/platform/browser';
 
 @Component({
 	selector: 'home',
@@ -13,7 +11,6 @@ import {DashboardComponent} from '../dashboard/dashboard.component';
 })
 @RouteConfig([
 	{path: '/', component:DashboardComponent, as: 'Dashboard', useAsDefault:true}
-	// {path: '/report', component:ReportComponent, as: 'Report'}
 ])
 
 export class HomeComponent {
