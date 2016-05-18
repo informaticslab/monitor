4e5026a2621bdc9391c132fa72f012236a579167
Michael Ta <technical.ta@gmail.com>
2016-05-18 14:34:28 -0400

    rc1 working need to merge

--- src/client/app/components/app/app.component.ts
+++ src/client/app/components/app/app.component.ts
@@ -2,11 +2,11 @@ import {Component} from '@angular/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {HTTP_PROVIDERS} from '@angular/http';
 
-// import {MonitorService, Service} from '../../services/hosts.service';
-// import {ReportService, Report} from '../../services/report.service';
-// import {WeatherReport, WeatherService} from '../../services/weather.service';
-// import {JiraReservations, JiraService} from '../../services/jira.service';
-// import {SensorData, ServerSensorService} from '../../services/server-sensor.service';
+import {MonitorService, Service} from '../../services/hosts.service';
+import {ReportService, Report} from '../../services/report.service';
+import {WeatherReport, WeatherService} from '../../services/weather.service';
+import {JiraReservations, JiraService} from '../../services/jira.service';
+import {SensorData, ServerSensorService} from '../../services/server-sensor.service';
 import {HomeComponent} from '../home/home.component';
 // import {LoginComponent} from '../login/login.component';
 
@@ -20,11 +20,11 @@ import {HomeComponent} from '../home/home.component';
 	providers: [
 		ROUTER_PROVIDERS,
 		HTTP_PROVIDERS,
-		// MonitorService,
-		// ReportService,
-		// WeatherService,
-		// JiraService,
-		// ServerSensorService
+		MonitorService,
+		ReportService,
+		WeatherService,
+		JiraService,
+		ServerSensorService
 	]
 })
 
