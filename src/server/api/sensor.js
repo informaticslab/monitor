27c7364d594855ad8edecb3ccb29b363199c6b65
Michael Ta <technical.ta@gmail.com>
2016-08-01 16:49:23 -0400

    change to hit PI directly

--- src/server/api/sensor.js
+++ src/server/api/sensor.js
@@ -6,7 +6,7 @@ module.exports.getRoutes = function() {
 	var router = express.Router();
 
 	router.get('/data', function(req, res){
-		var url ='http://172.16.100.228:3000/sensorData';
+		var url ='http://96.80.37.110:3000/sensorData';
 
 		request(url, function(error, response, body) {
 			if(!error && response.statusCode === 200) {
